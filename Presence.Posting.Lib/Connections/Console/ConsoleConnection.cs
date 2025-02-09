ea8cc9efe452a9b0ebe21511ba3d4ce0478c2c17
Lewis Westbury <lewis@cantab.net>
2025-02-09 14:35:31 +0000

    Console connection should write to stderr, not stdout - so as to be able to capture the summary json ALONE on stdout

--- Presence.Posting.Lib/Connections/Console/ConsoleConnection.cs
+++ Presence.Posting.Lib/Connections/Console/ConsoleConnection.cs
@@ -16,7 +16,7 @@ public class ConsoleConnection : AbstractNetworkConnection
     public override async Task<INetworkPostReference> PostAsync(CommonPost post, INetworkPostReference? replyTo = default)
     {
         var key = Guid.NewGuid().ToString();
-        System.Console.WriteLine($"{Account[NetworkCredentialType.PrintPrefix]} ({key}): {(replyTo != null ? $"(reply to: {replyTo.NetworkReferences["guid"]}) " : "")}{post.ComposeText()}");
+        System.Console.Error.WriteLine($"{Account[NetworkCredentialType.PrintPrefix]} ({key}): {(replyTo != null ? $"(reply to: {replyTo.NetworkReferences["guid"]}) " : "")}{post.ComposeText()}");
         return new ConsolePostReference
         {
             NetworkReferences = new Dictionary<string, string?>()
@@ -29,19 +29,19 @@ public class ConsoleConnection : AbstractNetworkConnection
 
     public override async Task<bool> DeletePostAsync(INetworkPostReference uri)
     {
-        System.Console.WriteLine($"{Account[NetworkCredentialType.PrintPrefix]} Deletion ({uri.NetworkReferences["guid"]})");
+        System.Console.Error.WriteLine($"{Account[NetworkCredentialType.PrintPrefix]} Deletion ({uri.NetworkReferences["guid"]})");
         return true;
     }
 
     protected override async Task<bool> ConnectImplementationAsync(INetworkAccount account)
     {
-        System.Console.WriteLine($"{Account[NetworkCredentialType.PrintPrefix]} Connected: {account.AccountPrefix}");
+        System.Console.Error.WriteLine($"{Account[NetworkCredentialType.PrintPrefix]} Connected: {account.AccountPrefix}");
         return true;
     }
 
     protected override async Task DisconnectImplementationAsync()
     {
-        System.Console.WriteLine($"{Account[NetworkCredentialType.PrintPrefix]} Disconnect");
+        System.Console.Error.WriteLine($"{Account[NetworkCredentialType.PrintPrefix]} Disconnect");
     }
 
 }
